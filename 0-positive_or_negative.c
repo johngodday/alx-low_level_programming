81940b6a076f2f84ec43d0abf5c3814e2c3a3621
johngodday <talk2john40@gmail.com>
2022-03-11 14:21:08 -0800

    commit

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Print a random number
+*
+*Return: program runs succefully
+*/
+
+int main(void)
+
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+
+	return (0);
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Programming in low level language
