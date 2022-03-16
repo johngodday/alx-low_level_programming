d768b679581a0bca046999ec9c42cec5b516e668
johngodday <talk2john40@gmail.com>
2022-03-15 23:15:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
-int main(){
-	char text[] = "_putchar";
-	putchar("%c", text);
-	return (0);
+#include "main.h"
+/**
+ *main - entry point.
+ *Description: program starts and ends here.
+ *Return: 0 on success.
+ */
+int main(void)
+{
+char text[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
+_putchar('\n');
+return (0);
 }
